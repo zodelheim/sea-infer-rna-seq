11af6220f0245e83d85a16c6057d54106547a73f
Arsenii Dokuchaev <zodelheim@gmail.com>
2024-07-21 14:33:47 +0200

    add pseudoautosomes drop

--- rnaseqanalysis/preprocessing/locate_sex_transcripts.py
+++ rnaseqanalysis/preprocessing/locate_sex_transcripts.py
@@ -8,22 +8,45 @@ fdir_processed = Path("/home/ar3/Documents/PYTHON/RNASeqAnalysis/data/interim")
 
 
 gtf_rawdata = read_gtf(fdir_raw / 'all_transcripts_strigtie_merged.gtf')
-
 gtf_data = gtf_rawdata.to_pandas()
 
-gtf_data = gtf_data[['seqname', 'transcript_id']]
-transcripts_x = gtf_data.loc[gtf_data['seqname'] == 'chrX', 'transcript_id']
-transcripts_y = gtf_data.loc[gtf_data['seqname'] == 'chrY', 'transcript_id']
-transcripts_x = transcripts_x.unique()
-transcripts_y = transcripts_y.unique()
+gtf_data = gtf_data[['seqname', 'transcript_id', 'start', 'end']]
+transcripts_x = gtf_data.loc[gtf_data['seqname'] == 'chrX']
+transcripts_y = gtf_data.loc[gtf_data['seqname'] == 'chrY']
+
+# drop pseudoautosomes
+pseudoautosoms_Y1 = [10001, 2781479]
+pseudoautosoms_X1 = [10001, 2781479]
+pseudoautosoms_Y2 = [56887903, 57217415]
+pseudoautosoms_X2 = [155701383, 156030895]
+
+true_transcripts_x = transcripts_x.loc[((transcripts_x['end'] < pseudoautosoms_X1[0])
+                                        | ((transcripts_x["start"] > pseudoautosoms_X1[1]) & (transcripts_x["end"] < pseudoautosoms_X2[0]))
+                                        | (transcripts_x["start"] > pseudoautosoms_X2[1])
+                                        )]
+
+true_transcripts_y = transcripts_y.loc[((transcripts_y['end'] < pseudoautosoms_Y1[0])
+                                        | ((transcripts_y["start"] > pseudoautosoms_Y1[1]) & (transcripts_y["end"] < pseudoautosoms_Y2[0]))
+                                        | (transcripts_y["start"] > pseudoautosoms_Y2[1])
+                                        )]
+
+transcripts_x = transcripts_x['transcript_id'].unique()
+transcripts_y = transcripts_y['transcript_id'].unique()
+
+true_transcripts_x = true_transcripts_x['transcript_id'].unique()
+true_transcripts_y = true_transcripts_y['transcript_id'].unique()
 
 print(len(transcripts_x))
 print(len(transcripts_y))
-
+print(len(true_transcripts_x))
+print(len(true_transcripts_y))
 
 pd.Series(transcripts_x).to_csv(fdir_processed / "all_transcripts.chrX.csv")
 pd.Series(transcripts_y).to_csv(fdir_processed / "all_transcripts.chrY.csv")
+pd.Series(true_transcripts_x).to_csv(fdir_processed / "true_transcripts.chrX.csv")
+pd.Series(true_transcripts_y).to_csv(fdir_processed / "true_transcripts.chrY.csv")
 
+exit()
 # ------------------ test if transcripts from geuvadis are in gencode v44 ---------------
 # print(gtf_data.loc[gtf_data['transcript_id'] == "MSTRG.35846.29"])
 
