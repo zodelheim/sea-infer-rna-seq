72c4a8baf820664561ad85048a1ae23dfdcecb0f
Arsenii Dokuchaev <zodelheim@gmail.com>
2024-11-07 12:37:43 +0100

    update make_eval_dataset as python module

--- rnaseqanalysis/preprocessing/make_eval_dataset.py
+++ rnaseqanalysis/preprocessing/make_eval_dataset.py
@@ -31,37 +31,38 @@ def logarithmization(df: pd.DataFrame):
     return df
 
 
-fdir_raw = Path("data/raw/")
-fdir_processed = Path("data/interim")
-fdir_traintest = Path("data/processed")
-fdir_external = Path("data/external")
+if __name__ == "__main__":
+    fdir_raw = Path("data/raw/")
+    fdir_processed = Path("data/interim")
+    fdir_traintest = Path("data/processed")
+    fdir_external = Path("data/external")
 
-for organ in ["BLOOD1", 'BRAIN0', "HEART", "BRAIN1"]:
-# for organ in ['BRAIN0']:
+    for organ in ["BLOOD1", 'BRAIN0', "HEART", "BRAIN1"]:
+        # for organ in ['BRAIN0']:
 
-    # fname = Path("heart.merged.TPM.txt")
-    if "BRAIN1" in organ:
-        fname = next((fdir_external / organ / 'reg').glob("*.csv"))
-        fname = fname.name
-        print(fname)
-        data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep=',', index_col=0).T
-    elif "BRAIN0" in organ:
-        fname = next((fdir_external / organ / 'reg').glob("*TPM.csv"))
-        fname = fname.name
-        print(fname)
-        data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep=',', index_col=0).T
-    else:
-        fname = next((fdir_external / organ / 'reg').glob("*TPM.txt"))
-        fname = fname.name
-        print(fname)
-        data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep='\t').T
+        # fname = Path("heart.merged.TPM.txt")
+        if "BRAIN1" in organ:
+            fname = next((fdir_external / organ / 'reg').glob("*.csv"))
+            fname = fname.name
+            print(fname)
+            data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep=',', index_col=0).T
+        elif "BRAIN0" in organ:
+            fname = next((fdir_external / organ / 'reg').glob("*TPM.csv"))
+            fname = fname.name
+            print(fname)
+            data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep=',', index_col=0).T
+        else:
+            fname = next((fdir_external / organ / 'reg').glob("*TPM.txt"))
+            fname = fname.name
+            print(fname)
+            data = pd.read_csv(fdir_external / organ / 'reg' / fname, sep='\t').T
 
-    data_header = pd.read_csv(fdir_external / organ / 'reg' / 'SraRunTable.txt', sep=',')
-    # print(data_header.columns)
-    data = filter_zero_median(data)
-    print(data.shape)
-    data = logarithmization(data)
-    data = data.astype(np.float32)
-    print(data)
-    data.to_hdf((fdir_external / organ / 'reg' / fname).with_suffix('.processed.h5'),
-                key='processed', format='f')
+        data_header = pd.read_csv(fdir_external / organ / 'reg' / 'SraRunTable.txt', sep=',')
+        # print(data_header.columns)
+        data = filter_zero_median(data)
+        print(data.shape)
+        data = logarithmization(data)
+        data = data.astype(np.float32)
+        print(data)
+        data.to_hdf((fdir_external / organ / 'reg' / fname).with_suffix('.processed.h5'),
+                    key='processed', format='f')
