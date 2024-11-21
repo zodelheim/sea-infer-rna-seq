2bb75a0484874fff8b79ebb58149be73aa51dbb4
Arsenii Dokuchaev <zodelheim@gmail.com>
2024-11-21 12:47:09 +0100

    update feature selection according to adata format

--- rnaseqanalysis/models/feature_selection.py
+++ rnaseqanalysis/models/feature_selection.py
@@ -5,25 +5,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, RobustScaler
 import argparse
-from sklearn.model_selection import train_test_split
 from catboost import CatBoostClassifier
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold, RepeatedStratifiedKFold
 from pathlib import Path
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, RocCurveDisplay, auc
-from sklearn.feature_selection import RFECV
-import mlflow
-import shap
 import json
 import cupy
+import anndata as ad
+
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from config import FDIR_EXTERNAL, FDIR_INTEMEDIATE, FDIR_PROCESSED, FDIR_RAW
 
 from tqdm import tqdm
 
-fdir_raw = Path("data/raw/")
-fdir_processed = Path("data/interim")
-fdir_traintest = Path("data/processed") / 'sex'
-fdir_external = Path("data/external")
+fdir_raw = FDIR_RAW
+fdir_intermediate = FDIR_INTEMEDIATE
+fdir_processed = FDIR_PROCESSED / 'sex'
+fdir_external = FDIR_EXTERNAL
 ml_models_fdir = Path("models")
 
 
@@ -31,6 +30,7 @@ use_CV = True
 
 model_type = 'catboost'
 model_type = 'xgboost'
+# model_type = "random_forest"
 
 # sex = 'chrXY'
 # sex = 'autosome'
@@ -50,33 +50,41 @@ organ_names = {
     'BRAIN0': "BRAIN0",
     "HEART": "HEART",
     "BRAIN1": "BRAIN1",
-    'None': "BLOOD"
+    'None': "BLOOD",
+    'CAGE.heart': 'CAGE.heart'
+}
+
+filename_prefixes = {
+    "None": "geuvadis",
+    'CAGE.heart': "CAGE.heart"
 }
 
 
 n_threads = 6
 
-value_to_predict = 'Sex'
+value_to_predict = 'sex'
 # value_to_predict = 'Experimental_Factor:_population (exp)'
 
-# for organ in ['BRAIN0']:
-for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
-    for sex_chromosome in ['chrXY', 'autosome', 'chrX', 'chrY']:
-        # for sex_chromosome in ['chrY']:
+# for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
+# for organ in ['CAGE.heart']:
+for organ in ['None']:
+    for sex_chromosome in ['chr_aXY']:
+        # for sex_chromosome in ['chr_aXY', 'autosomes', 'chr_aX', 'chr_aY']:
 
-        with open(f'models/{model_type}.json', 'r') as file:
-            model_params = json.load(file)
+        with open(f'models/model_params.json', 'r') as file:
+            model_params = json.load(file)[model_type]
+        model_params = model_params[value_to_predict]
 
         # print(model_params)
-        data = pd.read_hdf(fdir_traintest / f'geuvadis.preprocessed.sex.h5', key=sex_chromosome)
-        data_header = pd.read_hdf(fdir_processed / 'geuvadis.preprocessed.h5', key="header")
+        adata = ad.read(fdir_processed / f"GEUVADIS.preprocessed.{value_to_predict}.h5ad")
+        adata = adata[:, adata.varm[sex_chromosome]]
 
-        feature_importance_df = pd.read_hdf(fdir_processed / f'feature_importance.{model_type}.{value_to_predict}.organ_{organ}.h5',
+        feature_importance_df = pd.read_hdf(fdir_intermediate / f'feature_importance.{model_type}.{value_to_predict}.organ_{organ}.h5',
                                             key=f'{sex_chromosome}',)
 
         features = feature_importance_df[feature_importance_method]
 
-        if organ != "None":
+        if organ not in ["None", 'CAGE.heart']:
             fname = next((fdir_external / organ / 'reg').glob("*processed.h5"))
             fname = fname.name
 
@@ -96,11 +104,10 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
         for i in tqdm(range(1, max_n_features)):
             n_features = i
 
-            data_shrinked = data[features[:n_features]]
-            data_header_shrinked = data_header.loc[data.index]
+            data_shrinked = adata[:, features[:n_features]]
 
-            X = data_shrinked.values
-            y = data_header_shrinked['Sex']
+            X = data_shrinked.X
+            y = data_shrinked.obs[value_to_predict]
 
             label_encoder = LabelEncoder().fit(y)
             y = label_encoder.transform(y)
@@ -112,8 +119,8 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
             precision_array = []
             recall_array = []
 
-            cv = StratifiedKFold(n_splits=5)
-            # cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10)
+            # cv = StratifiedKFold(n_splits=5, shuffle=True)
+            cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10)
             # X_train, X_test, y_train, y_test = train_test_split(
             #     X, y, test_size=test_size, random_state=random_state, shuffle=True)
 
@@ -126,11 +133,8 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
                 # test_size = 0.2
                 # random_state = 42
 
-                train_scaler = RobustScaler().fit(X_train)
-                test_scaler = RobustScaler().fit(X_test)
-
-                X_train = train_scaler.transform(X_train)
-                X_test = test_scaler.transform(X_test)
+                X_train = RobustScaler().fit_transform(X_train)
+                X_test = RobustScaler().fit_transform(X_test)
 
                 # label_encoder = LabelEncoder().fit(y_train)
                 # y_train = label_encoder.transform(y_train)
@@ -152,6 +156,8 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
                     model = xgb.XGBClassifier(**model_params)
                     model.fit(cupy.array(X_train_), y_train_, eval_set=[(X_val, y_val)], verbose=False)
 
+                    X_test_c = cupy.array(X_test)
+
                 if model_type == 'catboost':
                     model = CatBoostClassifier(**model_params)
                     model.fit(X_train_, y_train_,
@@ -161,18 +167,24 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
                               plot=False,
                               early_stopping_rounds=20)
 
+                if model_type == 'random_forest':
+                    model = RandomForestClassifier()
+                    model.fit(X_train, y_train)
+
+                    X_test_c = X_test
+
                 # if not (ml_models_fdir / 'xgboost').is_dir():
                 #     (ml_models_fdir / 'xgboost').mkdir()
 
                 # saved_model_filename = f"geuvadis_{sex}.json"
                 # model.save_model(fname=ml_models_fdir / 'xgboost' / saved_model_filename)
 
-                roc_array.append(roc_auc_score(y_test, model.predict(cupy.array(X_test))))
+                roc_array.append(roc_auc_score(y_test, model.predict(X_test_c)))
 
-                accuracy_array.append(accuracy_score(y_test, model.predict(cupy.array(X_test))))
-                f1_array.append(f1_score(y_test, model.predict(cupy.array(X_test))))
-                precision_array.append(precision_score(y_test, model.predict(cupy.array(X_test))))
-                recall_array.append(recall_score(y_test, model.predict(cupy.array(X_test))))
+                accuracy_array.append(accuracy_score(y_test, model.predict(X_test_c)))
+                f1_array.append(f1_score(y_test, model.predict(X_test_c)))
+                precision_array.append(precision_score(y_test, model.predict(X_test_c)))
+                recall_array.append(recall_score(y_test, model.predict(X_test_c)))
 
             roc_array_total[i] = roc_array
 
@@ -202,7 +214,7 @@ for organ in ['BRAIN0', "HEART", "BRAIN1", 'None']:
         plt.ylabel('score value')
         plt.xlabel('# transcripts')
         plt.legend()
-        plt.savefig(f'reports/figures/nfeatures/geuvadis_{sex_chromosome}_organ_{organ}.png', dpi=300)
+        plt.savefig(f'reports/figures/nfeatures/{filename_prefixes[organ]}_{sex_chromosome}_organ_{organ}.png', dpi=300)
         plt.close()
         # plt.show()
         # exit()
